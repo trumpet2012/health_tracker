8e63598a1b0c7da0cb36a023e5b4d8191655eecd
Alex Channell <achannell@morristechnology.com>
2015-11-10 09:38:46 -0500

    added basic profile page, must be logged in to view it (because it gets the request.user). need to change this later to view any profile

--- health_records/urls.py
+++ health_records/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from .views import ProfileListing
 
+from health_records import views
+
 urlpatterns = [
-    url('^all/$', ProfileListing.as_view(), name='profile_listing')
+    url('^all/$', ProfileListing.as_view(), name='profile_listing'),
+    url('^profile/$', views.profile_page, name='profile_page'),
 ]
--- health_records/views.py
+++ health_records/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import ListView
 
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+
 from .models import HealthProfile
 
 
@@ -7,3 +11,17 @@ class ProfileListing(ListView):
     model = HealthProfile
     template_name = 'health_records/profile_listing.html'
 
+
+def profile_page(request):
+    template = 'health_records/user_profile.html'
+    username = request.user
+    try:
+        profile = HealthProfile.objects.get(user__username=username)
+    except ObjectDoesNotExist:
+        return HttpResponse("<h1>Must be logged in to view your profile (this should change later)</h1>")
+
+    context = {
+        'profile': profile
+    }
+
+    return render_to_response(template, context)
